7b484331299528bcb3cac8cfd5eb7bf28873d36a
jordan-cen-uci <jwcen@uci.edu>
2024-01-22 00:36:29 -0800

    import simulation zip

---
+++ Robot_Simulation_W2024/robot_simulation.m
@@ -0,0 +1,24 @@
+addpath('Setup Simulation');
+addpath('Execute Simulation');
+addpath('Execute Simulation/Control');
+addpath('Plot');
+
+% load parameters
+robot = load_physical_parameters();
+control = load_control_parameters();
+simulation_params = load_simulation_parameters();
+impulse_response = load_impulse_response(robot, simulation_params);
+
+% initialize variables
+sim = initialize_simulation_data(robot, control, impulse_response, simulation_params);
+
+% go through the simulation and control the robot
+for n = 2:length(sim.time)
+    sim = update_control(sim);
+    sim = update_simulation(sim);
+end
+
+% plot results and animation
+plot_animation(sim);
+plot_performance(sim);
+plot_impulse_response(sim);
