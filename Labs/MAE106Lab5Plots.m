fa1565cdb0618cbe009d2e8e8d17f29fbb8ac6c6
jordan-cen-uci <jwcen@uci.edu>
2024-03-06 22:10:26 -0800

    Lab5plot3

--- Labs/MAE106Lab5Plots.m
+++ Labs/MAE106Lab5Plots.m
@@ -50,6 +50,7 @@ ylabel('Position (radians)');
 xticks([0 0.05 0.10 0.15 0.2]);
 
 part2UndampedFreq = [1;2;4;5;6;8;12;16];
+part2UndampedAngFreq = pi .* part2UndampedFreq;
 part2UndampedInput = [1;1;1;1;0.9995;0.999;1;1];
 part2UndampedOutput = [0.941;0.907;0.995;0.9075;0.812;0.494;0.3385;0.2235];
 
@@ -61,16 +62,29 @@ part2UndampedPhase = [-36.288;-43.632;-43.632;-58.32;-78.624;-122.112;-121.824;-
 
 figure(4);
 clf;
-subplot(2,1,1);
-plot(part2UndampedFreq, part2UndampedAmp);
-grid on;
 subplot(2,1,2);
-plot(part2UndampedFreq, part2UndampedPhase);
+semilogx(part2UndampedAngFreq, part2UndampedPhase);
 hold on;
 grid on;
+ax5 = gca;
+ax5.XLim = [0 pi.*16];
+ax5.YLim = [-180 -20];
+ylabel('Phase Shift (degrees)');
+xlabel('Frequency (rad/s)');
 
+subplot(2,1,1);
+title('Undamped Bode Plot');
+semilogx(part2UndampedAngFreq, part2UndampedAmp);
+grid on;
+ax4 = gca;
+ax4.XLim = [0 pi.*16];
+ax4.YLim = [-15 3];
+ylabel('Magnitude (dB)');
+
+title('Undamped Bode Plot');
 
 part2DampedFreq = [1;2;4;5;6;8;12;16];
+part2DampedAngFreq = pi .* part2DampedFreq;
 part2DampedOutput = [1;1;0.9995;1;1;0.999;0.9995;1];
 part2DampedInput = [0.7445;0.894;0.9005;0.88;0.7855;0.5755;0.2435;0.2775];
 
@@ -78,6 +92,25 @@ part2DampedTrans = part2DampedInput./part2DampedOutput;
 
 part2DampedAmp = 20.*log10(part2DampedTrans);
 
+part2DampedPhase = [-21.888;-43.776;-43.488;-51.12;-65.664;-87.552;-126.576;-145.728];
+
 figure(5);
 clf;
-semilogx(part2DampedFreq, part2DampedAmp);
+
+subplot(2,1,2);
+semilogx(part2DampedAngFreq, part2DampedPhase);
+hold on;
+grid on;
+ax7 = gca;
+ax7.XLim = [0 pi.*16];
+ax7.YLim = [-180 -20];
+ylabel('Phase Shift (degrees)');
+xlabel('Frequency (rad/s)');
+subplot(2,1,1);
+semilogx(part2DampedAngFreq, part2DampedAmp);
+grid on;
+ax6 = gca;
+ax6.XLim = [0 pi.*16];
+ax6.YLim = [-15 3];
+ylabel('Magnitude (dB)');
+title('Critically Damped Bode Plot (Kd = 0.2)');
